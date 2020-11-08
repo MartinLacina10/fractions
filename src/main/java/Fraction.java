638f7c86952f949d7d091169d73601c3ed65d184
MartinLacina10 <58588480+MartinLacina10@users.noreply.github.com>
2020-11-09 00:24:17 +0100

    Fractions

--- src/main/java/Fraction.java
+++ src/main/java/Fraction.java
@@ -3,45 +3,25 @@ public class Fraction implements IFraction {
     private final Integer numerator;
     private final Integer denominator;
 
-    public Fraction(Integer numerator, Integer denominator) {
+    public Fraction (Integer numerator, Integer denominator) {
         this.numerator = numerator;
         this.denominator = denominator;
 
         if (denominator == 0) throw new ArithmeticException("Cannot Divide by 0");
     }
 
-    @Override
-    public Integer getNumerator() {
-        return numerator;
-    }
-
-    @Override
-    public Integer getDenominator() {
-        return denominator;
-    }
-
-    @Override
-    public IFraction plus(IFraction other) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public IFraction minus(IFraction other) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public IFraction times(IFraction other) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public IFraction dividedBy(IFraction other) {
-        throw new UnsupportedOperationException();
-    }
-
     public static Fraction createNormalised(Integer numerator, Integer denominator) {
-        throw new UnsupportedOperationException();
+        try {
+            for (int i = denominator / 2; i > 0; i--) {
+                if (denominator % i == 0 && numerator % i == 0) {
+                    return new Fraction (numerator / i, denominator / i);
+                }
+            }
+            throw new Exception();
+        } catch (Exception e) {
+            return new Fraction (numerator, denominator);
+        }
+
     }
 
     /**
@@ -56,9 +36,55 @@ public class Fraction implements IFraction {
     private static Integer findLowestCommonMultiple(Integer i1, Integer i2) {
         if (i1 == 0 || i2 == 0) return 0;
         else {
-            int gcd = findGreatestCommonDenominator(i1, i2);
-            return Math.abs(i1 * i2) / gcd;
+            int GreatCommonDenominator = findGreatestCommonDenominator(i1, i2);
+            return Math.abs(i1 * i2) / GreatCommonDenominator;
         }
+    }
+
+    @Override
+    public Integer getNumerator() {
+        return numerator;
+    }
+
+    @Override
+    public Integer getDenominator() {
+        return denominator;
+    }
+
+    @Override
+    public IFraction plus(IFraction other) {
+        int newDenominator = getDenominator();
+        int newNumerator = getNumerator();
+        int thisNumerator = other.getNumerator();
+        int thisDenominator = other.getDenominator();
+        return new Fraction ((newNumerator * thisDenominator) + (newDenominator * thisNumerator), newDenominator * thisDenominator);
+    }
+
+    @Override
+    public IFraction minus(IFraction other) {
+        int newDenominator = getDenominator();
+        int newNumerator = getNumerator();
+        int thisNumerator = other.getNumerator();
+        int thisDenominator = other.getDenominator();
+        return new Fraction ((newNumerator * thisDenominator) - (newDenominator * thisNumerator), newDenominator * thisDenominator);
+    }
+
+    @Override
+    public IFraction times(IFraction other) {
+        int newDenominator = getDenominator();
+        int newNumerator = getNumerator();
+        int thisNumerator = other.getNumerator();
+        int thisDenominator = other.getDenominator();
+        return new Fraction (newNumerator * thisNumerator, newDenominator * thisDenominator);
+    }
+
+    @Override
+    public IFraction dividedBy(IFraction other) {
+        int newDenominator = getDenominator();
+        int newNominator = getNumerator();
+        int thisNumerator = other.getNumerator();
+        int thisDenominator = other.getDenominator();
+        return new Fraction (newNominator * thisDenominator, newDenominator * thisNumerator);
     }
 
     @Override
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Fraction.createNormalised(120, 12));
+        System.out.println(Fraction.createNormalised(22, 12));
         System.out.println(Fraction.createNormalised(4, 12));
         System.out.println(new Fraction(2, 4).plus(new Fraction(3, 9)));
         System.out.println(new Fraction(2, 4).minus(new Fraction(3, 9)));
